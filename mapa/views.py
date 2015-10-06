56aaaaf1808a4672f10613a27457cddc0e1e3a2e
Maor Levy <levym10@gmail.com>
2015-10-06 19:58:14 -0300

    parametros de filtracion estaticos, hay que encapsular la filtracion por causa y por edades

--- mapa/views.py
+++ mapa/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from mapa.models import CountryCode
-from mapa.models import Mort
+from mapa.models import CountryCode, Mort
 from django.db.models import Sum
 
 # Create your views here.
 def index(request):
-    diccionario = {}
-    for c in CountryCode.objects.filter(name__startswith='A'):
-        total = 0
-        diccionario[c.name] = Mort.objects.filter(country=c.country).aggregate(Sum('deaths1'))
+    query = Mort.objects.filter(year = 2010).filter(cause='AAA').filter(sex='2')
+    #for c in CountryCode.objects.filter(name__startswith='J'):
+        #[c.name] = Mort.objects.filter(country=c.country).aggregate(Sum('deaths1'))
         #for q in Mort.objects.raw('SELECT id, deaths1 FROM mort WHERE country = %s',[c.country]):
-        #for q in Mort.objects.filter(country=c.country):
-        #    total += q.deaths1
-        #diccionario[c.name] = total
+    diccionario = generarDic(query)
     context = {"test" : diccionario}
     return render(request, 'mapa/index.html',context)
+
+def generarDic(query):
+    dic = {}
+    for c in CountryCode.objects.filter(name__startswith='A'):
+        temp = query.filter(country=c.country).aggregate(Sum('deaths1'))
+        dic[c.name] = temp['deaths1__sum']
+    return dic
