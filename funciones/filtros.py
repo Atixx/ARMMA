9925aaef5ae6b50829aca72d47127fdab1417b8a
Maor Levy <levym10@gmail.com>
2015-10-13 00:58:48 -0300

    backend completo

--- funciones/filtros.py
+++ funciones/filtros.py
@@ -16,39 +16,6 @@ def tripleFiltro(anio, sexo, causaIni, causaFin = None):
             query = Mort.objects.filter(year = anio).filter(cause__range=(causaIni, causaFin)).exclude(cause='AAA')
     return query
 
-def poblacion(anio, sexo):
-    if (int(sexo) !=0):
-        pop = Pop.objects.filter(year=anio).filter(sex=sexo)
-    else:
-        pop = Pop.objects.filter(year=anio)
-    return pop
-
-def filtrarPaises(inicio, fin = None): #fin es excluyente, A-B no responderia con los paises de B
-    if fin is None:
-        paises =  CountryCode.objects.filter(name__startswith=inicio)
-    else:
-        paises =  CountryCode.objects.filter(name__range=(inicio,fin))
-    return paises
-
-
-def generarInfoPais(query, population, deathsSearch,popSearch):
-    #pdb.set_trace()
-
-
-
-    death = query.filter(country=c.country).aggregate(totalD=Sum(deathsSearch))['totalD']
-    pop = population.filter(country=c.country).aggregate(totalP=Sum(popSearch))['totalP']
-    if (death and pop):
-        dic[c.name] = ((death+0.0)/pop)
-    else:
-        dic[c.name] = None
-        #dic[c.country] = query.filter(country=c.country).aggregate(total=Sum(grupoEdad))['total']
-    #else: #Quedo para sumar 2 edades, no puede contemplar parametros variables
-    #    for c in CountryCode.objects.filter(name__startswith='A'):
-    #    dic[c.name] = query.filter(country=c.country).aggregate(total=Sum(grupoInicio)+Sum('deaths9'))['total']
-    #    query.aggregate(total=Sum('deaths10')+Sum('deaths9'))['total']
-    return dic
-
 def ultimoAnioPop(pais, anioReal):
     p = Pop.objects.filter(country=pais).filter(year__lt=anioReal).order_by('-year')
     if (p):
@@ -59,21 +26,40 @@ def ultimoAnioPop(pais, anioReal):
 #edades 0-8: segun la tabla
 def xPais(pais, anio, sexo, causaIni, causaFin, edades):
     retval = 0
-    if causaFin == None:
-        if (sexo != 0):
-            query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause=causaIni).filter(sex=sexo)
+    mortality = 0
+    popTotal = 0
+    anioPop = ultimoAnioPop(pais.country, anio)
+    if anioPop:
+        if causaFin == None:
+            if (int(sexo) != 0):
+                query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause=causaIni).filter(sex=sexo)
+                poblacion = Pop.objects.filter(country=pais.country).filter(year = anioPop).filter(sex=sexo)
+            else:
+                query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause=causaIni)
+                poblacion = Pop.objects.filter(country=pais.country).filter(year = anioPop)
         else:
-            query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause=causaIni)
+            if (int(sexo) != 0):
+                query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause__range=(causaIni, causaFin)).filter(sex=sexo).exclude(cause='AAA')
+                poblacion = Pop.objects.filter(country=pais.country).filter(year = anioPop).filter(sex=sexo)
+            else:
+                query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause__range=(causaIni, causaFin)).exclude(cause='AAA')
+                poblacion = Pop.objects.filter(country=pais.country).filter(year = anioPop)
+            poblacion = Pop.objects.filter(country=pais.country).filter(year = anioPop)
+        for l in formatoEdades(int(edades)):
+            find = 'deaths'+l
+            findPop = 'pop'+l
+            q = query.aggregate(total=Sum(find))['total']
+            p = poblacion.aggregate(total=Sum(findPop))['total']
+            if q:
+                mortality += q
+            if p:
+                popTotal += p
+        if q and p:
+            retval = (((q+0.0)/p)*1000)
+        else:
+            retval = None
     else:
-        if (sexo != 0):
-            query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause__range=(causaIni, causaFin)).filter(sex=sexo).exclude(cause='AAA')
-        else:
-            query = Mort.objects.filter(country=pais.country).filter(year = anio).filter(cause__range=(causaIni, causaFin)).exclude(cause='AAA')
-    for l in formatoEdades(int(edades)):
-        find = 'deaths'+l
-        q = query.aggregate(total=Sum(find))['total']
-        if q:
-            retval += q
+        retval = None
     return retval
 
 def formatoEdades(edades):
